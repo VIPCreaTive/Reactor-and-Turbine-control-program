2b23642fcf3af4d83fdcd5e6cb329677be181447
VIPCreaTive <philipp.auler@gmx.de>
2019-05-09 22:06:49 +0200

    Update turbineControl.lua

--- turbineControl_v2/src/program/turbineControl.lua
+++ turbineControl_v2/src/program/turbineControl.lua
@@ -975,7 +975,7 @@ function printStatsCoreAuto(cores)
 	for MonitorNumber=0,(amountMonitors -1) do
 		monitor[MonitorNumber].setBackgroundColor(backgroundColor)
 		monitor[MonitorNumber].setCursorPos(2, 1)
-		monitor[MonitorNumber].write("Test-Monitor-Nr: " .. (MonitorNumber))
+		monitor[MonitorNumber].write("Test-Monitor-Nr: " .. (cores))
 	
 	--refresh current turbine
     currStat = cores
@@ -985,7 +985,7 @@ function printStatsCoreAuto(cores)
 	monitor[MonitorNumber].write("lastStat: ".. lastStat .. "")
 	
     --toggles turbine buttons if pressed (old button off, new button on)
-    if not page.buttonList["*" .. currStat + 1].active then
+    if not page.buttonList["*" .. CurrStat + 1].active then
         page:toggleButton("*" .. currStat + 1)
     end
     if currStat ~= lastStat then
