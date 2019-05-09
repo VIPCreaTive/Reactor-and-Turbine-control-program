f7dd8dcbd33fa899347e4f73879de8e3bb020346
VIPCreaTive <philipp.auler@gmx.de>
2019-05-09 17:55:11 +0200

    Update turbineControl.lua

--- turbineControl_v2/src/program/turbineControl.lua
+++ turbineControl_v2/src/program/turbineControl.lua
@@ -681,7 +681,7 @@ function createAllButtons()
                 page:add("#" .. (i + 1), function() printStatsAuto(i) end, x1, y, x1 + 5, y)
             elseif (i > 7 and i <= 15) then
                 page:add("#" .. (i + 1), function() printStatsAuto(i) end, x2, y, x2 + 5, y)
-            end
+            end -- if-amount
 		
         elseif overallMode == "manual" then
             if i <= 7 then
@@ -694,8 +694,9 @@ function createAllButtons()
                 page:add("#" .. (i + 1), function() printStatsMan(i) end, x4, y, x4 + 5, y)
             end
 
-            if (i == 7 or i == 15 or i == 23) then y = 4
-				else y = y + 2
+            if (i == 7 or i == 15 or i == 23) then 
+				y = 4
+			else y = y + 2
             end
         end --if mode
     end --for
@@ -710,10 +711,12 @@ function createAllButtons()
 			--page:add("#" .. (i + 1), function() printStatsAuto(i) end, x3, y, x3 + 5, y)
 		--elseif i > 23 then
 			--page:add("#" .. (i + 1), function() printStatsAuto(i) end, x4, y, x4 + 5, y)
-			if (i == 7 or i == 15 or i == 23) then y = 4
-				else y = y + 2
+			if (i == 7 or i == 15 or i == 23) then 
+				y = 4
+			else y = y + 2
 			end
-	end --if
+		end --if amount
+	end --for
 	
     --Other buttons
     if lang == "de" then
