f1d2f9fed536ff239c84af814d6424f4bfb79357
VIPCreaTive <philipp.auler@gmx.de>
2019-05-09 17:58:34 +0200

    Update turbineControl.lua

--- turbineControl_v2/src/program/turbineControl.lua
+++ turbineControl_v2/src/program/turbineControl.lua
@@ -678,7 +678,7 @@ function createAllButtons()
     for i = 0, amountTurbines, 1 do
         if overallMode == "auto" then
             if i <= 7 then
-                page:add("#" .. (i + 1), function() printStatsAuto(i) end, x1, y, x1 + 5, y)
+                page:add("#" .. (i + 1), function() printStatsAuto(i) end, x1, y, x1 + 5, 4)
             elseif (i > 7 and i <= 15) then
                 page:add("#" .. (i + 1), function() printStatsAuto(i) end, x2, y, x2 + 5, y)
             end -- if-amount
@@ -704,18 +704,18 @@ function createAllButtons()
 	--Energycore buttons		
 	for i = 0, (amountEnergy -1 ), 1 do
 		if i <= 7 then
-			page:add("+" .. (i + 1), function() printStatsAuto(i) end, x3, y, x3 + 5, y)
+			page:add("+" .. (i + 1), function() printStatsAuto(i) end, x3, y, x3 + 5, 8)
 		elseif (i > 7 and i <= 15) then
 			page:add("+" .. (i + 1), function() printStatsAuto(i) end, x3, y, x3 + 5, y)
 		--elseif (i > 15 and i <= 23) then
 			--page:add("#" .. (i + 1), function() printStatsAuto(i) end, x3, y, x3 + 5, y)
 		--elseif i > 23 then
 			--page:add("#" .. (i + 1), function() printStatsAuto(i) end, x4, y, x4 + 5, y)
+		end --if amount
 			if (i == 7 or i == 15 or i == 23) then 
 				y = 4
 			else y = y + 2
 			end
-		end --if amount
 	end --for
 	
     --Other buttons
